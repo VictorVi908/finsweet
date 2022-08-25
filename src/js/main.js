d2fa28d05938fec2af290a5e9817622318518d9a
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2022-08-25 12:34:41 +0600

    делаю адаптив
    сделал меню сырбургер

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
 const benefits = document.querySelectorAll(".benefits__item");
+const menuButton = document.querySelector(".header__menu-btn");
+const contactButton = document.querySelector(".header__btn");
+const menu = document.querySelector(".menu");
 
 benefits.forEach(item => {
     const text = item.querySelector(".benefits__info-text")
@@ -14,7 +17,7 @@ benefits.forEach(item => {
             text.style.opacity = 0.78;
             text.style.height = textHeight + 'px';
         }, 0);
-        
+
         console.log(text.style.height)
 
     });
@@ -29,4 +32,26 @@ benefits.forEach(item => {
 });
 
 
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 860 && menu.classList.contains("menu--open") == true ) {
+        menu.classList.remove("menu--open");
+        menu.style.display = "";
+    };
+});
+
+menuButton.addEventListener('click', function () {
+    if (menu.classList.contains("menu--open") == false) {
+        menu.style.display = "block";
+        setTimeout(function () {
+            menu.classList.add("menu--open");
+        }, 10);
+        
+    } else {
+        menu.classList.remove("menu--open");
+        setTimeout(function () {
+            menu.style.display = "";
+        }, 200);
+    }
+});
 
